bfdf3bd2dc14aa79bd02f194a530e20642d4c4a5
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2024-12-17 22:39:14 -0500

    Adds custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import AppHeader from './components/Header';
 import { CafeDatabase } from './components/CafeDatabase';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme';
 
 function App() {
   return (
     <div className="App">
-      <AppHeader></AppHeader>
-      <CafeDatabase></CafeDatabase>
+      <ThemeProvider theme={theme}>
+        <AppHeader></AppHeader>
+        <CafeDatabase></CafeDatabase>
+      </ThemeProvider>
     </div>
   );
 }
