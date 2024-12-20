9bc6ddcf4f95825fd5d0ece5a566826573ffd7be
AnanyaT129 <AnaTadig@gmail.com>
2024-12-20 12:16:36 -0500

    barebones home page and router setup

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppHeader from '../components/Header';
+import HomeGrid from '../components/HomeGrid';
+
+function Home() {
+  return (
+    <div>
+      <AppHeader></AppHeader>
+      <HomeGrid></HomeGrid>
+    </div>
+  )
+}
+
+export default Home
